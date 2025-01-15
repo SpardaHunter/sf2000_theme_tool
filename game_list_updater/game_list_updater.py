d63b8b0175283c20ed1d02ad789afb612ed7e853
Sparda_Hunter <155873668+SpardaHunter@users.noreply.github.com>
2025-01-15 23:43:26 +0100

    Update game_list_updater.py

--- game_list_updater/game_list_updater.py
+++ game_list_updater/game_list_updater.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-import subprocess
+from game_list_updater.foldernamx import FrogtoolGUIx
+from game_list_updater.foldername import FrogtoolGUIe
 
 class GameListUpdater(tk.Frame):
     def __init__(self, root):
@@ -9,26 +10,19 @@ class GameListUpdater(tk.Frame):
         self.label = tk.Label(self, text="Game List Updater Module")
         self.label.pack(pady=20)
 
-        # Botón para abrir el programa relacionado con FoldernamX.ini
-        btn_12 = tk.Button(self, text="FoldernamX.ini", command=lambda: self.open_tool('game_list_updater/foldernamx.py'), width=25, height=2)
+        # Botón para mostrar la clase FrogtoolGUIx
+        btn_12 = tk.Button(self, text="FoldernamX.ini", command=lambda: self.show_frogtool('x'), width=25, height=2)
         btn_12.pack(pady=10)
 
-        # Botón para abrir el programa relacionado con Foldername.ini
-        btn_7 = tk.Button(self, text="Foldername.ini", command=lambda: self.open_tool('game_list_updater/foldername.py'), width=25, height=2)
+        # Botón para mostrar la clase FrogtoolGUIe
+        btn_7 = tk.Button(self, text="Foldername.ini", command=lambda: self.show_frogtool('e'), width=25, height=2)
         btn_7.pack(pady=10)
 
-        # Inicializar el proceso
-        self.current_process = None
-
-    def open_tool(self, script_path):
-        """Abrir un programa sin bloquear la GUI y cerrar cualquier proceso existente."""
-        # Cerrar el proceso en ejecución si existe
-        if self.current_process and self.current_process.poll() is None:
-            try:
-                self.current_process.terminate()
-                self.current_process.wait(timeout=5)
-            except Exception:
-                self.current_process.kill()
-
-        # Abrir el nuevo programa
-        self.current_process = subprocess.Popen(['python', script_path], shell=False)
+    def show_frogtool(self, version):
+        """Mostrar la interfaz correspondiente (FrogtoolGUIx o FrogtoolGUIe) dependiendo del parámetro."""
+        if version == 'x':
+            # Cambiar a FrogtoolGUIx
+            self.master.change_frame(FrogtoolGUIx(self.master))
+        elif version == 'e':
+            # Cambiar a FrogtoolGUIe
+            self.master.change_frame(FrogtoolGUIe(self.master))
